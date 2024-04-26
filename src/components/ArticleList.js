ade0e759e6796dd5cefbf22140941bc374b7c2b3
laurahfamba <cheroplaurah7@gmail.com>
2024-04-26 16:10:25 +0300

    completed lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,44 @@
+
+
+
+import React from "react";
+import Article from "./Article";
+
+function ArticleList() {
+
+  const posts = [
+    {
+      id: 1, 
+      title: "Components 101",
+      date: "December 15, 2020",
+      preview: "Setting up the building blocks of your site"
+    },
+    {
+      id: 2,
+      title: "React Data Flow",  
+      date: "December 11, 2020",
+      preview: "Passing props is never passé"
+    },
+    {
+      id: 3, 
+      title: "Function Components vs Class Components",
+      date: "December 10, 2020",
+      preview: "React, meet OOJS."
+    }
+  ];
+
+  return (
+    <main>
+      {posts.map(post => (
+        <Article 
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview} 
+        />  
+      ))}
+    </main>
+  )
+}
+
+export default ArticleList;
